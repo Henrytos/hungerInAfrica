3dda81661a45d4bd2776d33a8e9dfb85544e9b8e
Henry <franzhenry46@gmail.com>
2023-09-19 19:09:28 -0300

    ajustando os textos dos modais

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,61 +6,19 @@ import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Footer from "../components/Footer";
+
+import {
+  modalOne,
+  modalTwo,
+  modalThree,
+  modalFour,
+  modalFive,
+  modalSix,
+} from "./textModais";
 export default function Home() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const modalOne = {
-    title: "Processo Colonial",
-    text: [
-      " Durante a colonização, os países que ocuparam a África retiraram do território riquezas materiais e matérias-primas que poderiam servir para o desenvolvimento da região. Além disso, escravizou seus povos, retirando a população jovem que tinha condições de trabalhar.",
-      "No processo de descolonização, para conquistarem a independência, alguns países tiveram que lutar longamente contra seus colonizadores. Este foi o caso da Argélia e do Congo, por exemplo.",
-
-      "Além disso, devemos considerar os próprios conflitos internos dos povos africanos, que após a independência, entraram em guerra civil.",
-    ],
-    img: "/imgs/colonizacao-africa.jpg",
-  };
-  const modalTwo = {
-    title: "As Condições Climáticas",
-    text: [
-      " O continente é um dos mais afetados pelas mudanças climáticas nos últimos anos, que provocaram o aumento da incidência de secas, inundações e ciclones na região.",
-    ],
-    img: "/imgs/condicoeCLimaticas.jpg",
-  };
-  const modalThree = {
-    title: "A Concetração De Poder",
-    text: [
-      " No processo de descolonização, para conquistarem a independência, alguns países (no caso da Argélia e do Congo) tiveram que lutar contra seus colonizadores.",
-      "Devemos considerar os próprios conflitos internos dos povos africanos, que gerou o aumento da pobreza da população, resultando em muitos refugiados e pessoas desabrigadas, ampliando as crises econômica e política, fragilizando a infraestrutura territorial e ocasionando milhares de mortes.",
-    ],
-    img: "/imgs/cdp.jpg",
-  };
-  const modalFour = {
-    title: "A Corrupção DasAutoridades",
-    text: [
-      "As verbas de ajudas humanitárias com frequência acabam nas mãos de políticos corruptos e não chegam a quem precisa.",
-    ],
-    img: "/imgs/corrupcaoNaAfrica.jpg",
-  };
-  const modalOFive = {
-    title: "A Baixa Produtividade Agrícola",
-    text: [
-      "  Os problemas ambientais também aumentam a questão da fome. Hoje, a África carece de soluções para os processos de erosão e desertificação ocorridos pelo desmatamento. Áreas com solo pobre têm menores chances de produção agrícola e baixo desempenho.",
-      "As questões ambientais africanas esbarram na falta de investimentos e na competitividade. Os organismos internacionais atuam nas consequências do problema e não nas causas.",
-    ],
-    img: "/imgs/agricultura na africa.avif",
-  };
-  const modalSix = {
-    title: "O Aumento Populacional",
-    text: [
-      "A crise alimentar é favorecida, ainda, pelo aumento populacional. Ainda de acordo com a ONU, em 1950, a África era habitada por 221 milhões de habitantes.",
-      "O número passou para quase 1 bilhão em 2009. Isso se explica porque a África ainda é uma economia eminentemente rural e mais filhos significam mais braços para trabalhar.",
-
-      "Igualmente, existem poucos programas que permitam o planejamento familiar. Desta maneira, a taxa de nascimentos na África é de 5,2 nascimentos por mulher ao longo da vida e é a mais alta do mundo.",
-    ],
-    img: "/imgs/aumentoPolicional.jpg",
-  };
 
   const [targetModal, setTargetModal] = useState(modalOne);
 
@@ -263,7 +221,7 @@ export default function Home() {
               id="btnGalery"
               onClick={() => {
                 handleShow();
-                setTargetModal(modalOFive);
+                setTargetModal(modalFive);
               }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
---
+++ src/pages/textModais.js
@@ -0,0 +1,52 @@
+const modalOne = {
+  title: "Processo Colonial",
+  text: [
+    " Durante a colonização, os países que ocuparam a África retiraram do território riquezas materiais e matérias-primas que poderiam servir para o desenvolvimento da região. Além disso, escravizou seus povos, retirando a população jovem que tinha condições de trabalhar.",
+    "No processo de descolonização, para conquistarem a independência, alguns países tiveram que lutar longamente contra seus colonizadores. Este foi o caso da Argélia e do Congo, por exemplo.",
+
+    "Além disso, devemos considerar os próprios conflitos internos dos povos africanos, que após a independência, entraram em guerra civil.",
+  ],
+  img: "/imgs/colonizacao-africa.jpg",
+};
+const modalTwo = {
+  title: "As Condições Climáticas",
+  text: [
+    " O continente é um dos mais afetados pelas mudanças climáticas nos últimos anos, que provocaram o aumento da incidência de secas, inundações e ciclones na região.",
+  ],
+  img: "/imgs/condicoeCLimaticas.jpg",
+};
+const modalThree = {
+  title: "A Concetração De Poder",
+  text: [
+    " No processo de descolonização, para conquistarem a independência, alguns países (no caso da Argélia e do Congo) tiveram que lutar contra seus colonizadores.",
+    "Devemos considerar os próprios conflitos internos dos povos africanos, que gerou o aumento da pobreza da população, resultando em muitos refugiados e pessoas desabrigadas, ampliando as crises econômica e política, fragilizando a infraestrutura territorial e ocasionando milhares de mortes.",
+  ],
+  img: "/imgs/cdp.jpg",
+};
+const modalFour = {
+  title: "A Corrupção DasAutoridades",
+  text: [
+    "As verbas de ajudas humanitárias com frequência acabam nas mãos de políticos corruptos e não chegam a quem precisa.",
+  ],
+  img: "/imgs/corrupcaoNaAfrica.jpg",
+};
+const modalFive = {
+  title: "A Baixa Produtividade Agrícola",
+  text: [
+    "  Os problemas ambientais também aumentam a questão da fome. Hoje, a África carece de soluções para os processos de erosão e desertificação ocorridos pelo desmatamento. Áreas com solo pobre têm menores chances de produção agrícola e baixo desempenho.",
+    "As questões ambientais africanas esbarram na falta de investimentos e na competitividade. Os organismos internacionais atuam nas consequências do problema e não nas causas.",
+  ],
+  img: "/imgs/agricultura na africa.avif",
+};
+const modalSix = {
+  title: "O Aumento Populacional",
+  text: [
+    "A crise alimentar é favorecida, ainda, pelo aumento populacional. Ainda de acordo com a ONU, em 1950, a África era habitada por 221 milhões de habitantes.",
+    "O número passou para quase 1 bilhão em 2009. Isso se explica porque a África ainda é uma economia eminentemente rural e mais filhos significam mais braços para trabalhar.",
+
+    "Igualmente, existem poucos programas que permitam o planejamento familiar. Desta maneira, a taxa de nascimentos na África é de 5,2 nascimentos por mulher ao longo da vida e é a mais alta do mundo.",
+  ],
+  img: "/imgs/aumentoPolicional.jpg",
+};
+
+export { modalOne, modalTwo, modalThree, modalFour, modalFive, modalSix };
