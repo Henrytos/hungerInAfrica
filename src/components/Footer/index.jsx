4ca8e28f34e6ba4dcb948718c985e6b3bd32434a
Henry <franzhenry46@gmail.com>
2023-09-18 21:08:19 -0300

    finalizando prototipo

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#c7cec9] text-zinc-950 py-4 text-center">
+      Feito com Amor Pelo 2-DS &copy; 2023
+    </footer>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,65 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import Footer from "../components/Footer";
 export default function Home() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const modalOne = {
+    title: "Processo Colonial",
+    text: [
+      " Durante a colonização, os países que ocuparam a África retiraram do território riquezas materiais e matérias-primas que poderiam servir para o desenvolvimento da região. Além disso, escravizou seus povos, retirando a população jovem que tinha condições de trabalhar.",
+      "No processo de descolonização, para conquistarem a independência, alguns países tiveram que lutar longamente contra seus colonizadores. Este foi o caso da Argélia e do Congo, por exemplo.",
+
+      "Além disso, devemos considerar os próprios conflitos internos dos povos africanos, que após a independência, entraram em guerra civil.",
+    ],
+    img: "/imgs/colonizacao-africa.jpg",
+  };
+  const modalTwo = {
+    title: "As Condições Climáticas",
+    text: [
+      " O continente é um dos mais afetados pelas mudanças climáticas nos últimos anos, que provocaram o aumento da incidência de secas, inundações e ciclones na região.",
+    ],
+    img: "/imgs/condicoeCLimaticas.jpg",
+  };
+  const modalThree = {
+    title: "A Concetração De Poder",
+    text: [
+      " No processo de descolonização, para conquistarem a independência, alguns países (no caso da Argélia e do Congo) tiveram que lutar contra seus colonizadores.",
+      "Devemos considerar os próprios conflitos internos dos povos africanos, que gerou o aumento da pobreza da população, resultando em muitos refugiados e pessoas desabrigadas, ampliando as crises econômica e política, fragilizando a infraestrutura territorial e ocasionando milhares de mortes.",
+    ],
+    img: "/imgs/cdp.jpg",
+  };
+  const modalFour = {
+    title: "A Corrupção DasAutoridades",
+    text: [
+      "As verbas de ajudas humanitárias com frequência acabam nas mãos de políticos corruptos e não chegam a quem precisa.",
+    ],
+    img: "/imgs/corrupcaoNaAfrica.jpg",
+  };
+  const modalOFive = {
+    title: "A Baixa Produtividade Agrícola",
+    text: [
+      "  Os problemas ambientais também aumentam a questão da fome. Hoje, a África carece de soluções para os processos de erosão e desertificação ocorridos pelo desmatamento. Áreas com solo pobre têm menores chances de produção agrícola e baixo desempenho.",
+      "As questões ambientais africanas esbarram na falta de investimentos e na competitividade. Os organismos internacionais atuam nas consequências do problema e não nas causas.",
+    ],
+    img: "/imgs/agricultura na africa.avif",
+  };
+  const modalSix = {
+    title: "O Aumento Populacional",
+    text: [
+      "A crise alimentar é favorecida, ainda, pelo aumento populacional. Ainda de acordo com a ONU, em 1950, a África era habitada por 221 milhões de habitantes.",
+      "O número passou para quase 1 bilhão em 2009. Isso se explica porque a África ainda é uma economia eminentemente rural e mais filhos significam mais braços para trabalhar.",
+
+      "Igualmente, existem poucos programas que permitam o planejamento familiar. Desta maneira, a taxa de nascimentos na África é de 5,2 nascimentos por mulher ao longo da vida e é a mais alta do mundo.",
+    ],
+    img: "/imgs/aumentoPolicional.jpg",
+  };
+
+  const [targetModal, setTargetModal] = useState(modalOne);
+
   return (
     <>
       <main id="start">
@@ -69,7 +123,7 @@ export default function Home() {
             alt=""
             className="h-full w-[50%] object-cover max-lg:w-[80%] max-sm:w-[100%]"
           />
-          <div className="w-full max-w-sm text-base flex flex-col  gap-4 ">
+          <div className="w-full max-w-sm text-base flex flex-col  gap-4 max-lg:max-w-[80%] max-sm:max-w-full">
             <span className="text-4xl font-semibold">CAUSAS:</span>
             <span>
               Na África, a falta de comida resulta de vários fatores como o
@@ -114,7 +168,10 @@ export default function Home() {
             <span className="text-center text-2xl ">Processo Colonial</span>
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalOne);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -135,7 +192,10 @@ export default function Home() {
             </span>
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalTwo);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -155,7 +215,10 @@ export default function Home() {
             </span>
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalThree);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -177,7 +240,10 @@ export default function Home() {
             </span>
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalFour);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -196,7 +262,10 @@ export default function Home() {
             </span>
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalOFive);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -217,7 +286,10 @@ export default function Home() {
 
             <a
               id="btnGalery"
-              onClick={handleShow}
+              onClick={() => {
+                handleShow();
+                setTargetModal(modalSix);
+              }}
               className="text-center block m-auto  min-w-[8rem] text-sm
           py-3 border-2 border-[#485342]  rounded-lg
           hover:bg-[#485342] hover:text-[#c7cec9] transition-colors
@@ -230,7 +302,7 @@ export default function Home() {
       </Section>
       <Section className="bg-[#485342] text-zinc-50">
         <div className="flex gap-10 items-center h-[60vh] max-lg:flex-col max-lg:py-4 max-lg:h-[100vh]">
-          <div className="w-full max-w-sm text-base flex flex-col  gap-4">
+          <div className="w-full max-w-sm text-base flex flex-col  gap-4 max-lg:max-w-[80%] max-sm:max-w-full">
             <span className="text-4xl font-semibold">SOLUÇÕES:</span>
             <span>
               Uma administração correta, incentivo à agricultura familiar e a
@@ -250,23 +322,26 @@ export default function Home() {
           />
         </div>
       </Section>
-      <footer className="bg-[#c7cec9] text-zinc-950 py-4 text-center">
-        Feito com Amor Pelo 2-DS &copy; 2023
-      </footer>
+      <Footer />
 
       <Modal show={show} onHide={handleClose} animation={false}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>
+            <span className="text-[#485342]">{targetModal.title}</span>
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body className="flex flex-col gap-4">
+          <img
+            src={targetModal.img}
+            alt={targetModal.title}
+            className="m-auto max-w-xs w-full object-cover h-80"
+          />
+          <div className="flex flex-col gap-2">
+            {targetModal.text.map((text) => (
+              <span>{text}</span>
+            ))}
+          </div>
+        </Modal.Body>
       </Modal>
     </>
   );
